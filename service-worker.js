7f63d1f6684a7485fdb46ea5e57b8eec14aeb469
Denis-Andrei <denispeptanariu@gmail.com>
2019-10-02 19:51:33 +0100

    Updates

--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/clothing-store/precache-manifest.85c3769fb08d71c116ea7fff1764ba90.js"
+  "/clothing-store/precache-manifest.218f00f059eee12882b54056f7e7b092.js"
 );
 
 self.addEventListener('message', (event) => {
